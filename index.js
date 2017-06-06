631e08e34f8ef2685d4b508f20a1b168a5d93619
Gavin Boulton <gavboulton@gmail.com>
2017-06-06 12:07:07 +0100

    Write files to file system

---
+++ index.js
@@ -0,0 +1,25 @@
+const express = require('express');
+const tus = require('tus-node-server');
+const EVENTS = tus.EVENTS;
+
+const server = new tus.Server();
+server.datastore = new tus.FileStore({ path: '/files' });
+
+server.on(EVENTS.EVENT_FILE_CREATED, (e) => {
+  console.log('File created');
+  console.log(e.file);
+});
+
+server.on(EVENTS.EVENT_UPLOAD_COMPLETE, (e) => {
+  console.log('Upload complete');
+  console.log(e.file);
+});
+
+const app = express();
+app.all('/files/*', (req, res) => {
+  server.handle(req, res);
+});
+
+const PORT = process.env.PORT || 3003;
+app.listen(PORT);
+console.log(`App listening on ${PORT}`);
